6afe3b209dbae918c63813d2e63c648a9a431603
DavidMartinez95 <david.martinez.martin.1995@gmail.com>
2017-04-30 18:01:27 +0200

    Machine Learning scripts

---
+++ Assets/Code/MachineLearning/MLBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MLBehaviour : MonoBehaviour {
+
+	private Hashtable playerMatrix = new Hashtable();
+
+	private void Update() {
+		if (Input.GetKeyDown (KeyCode.F1)) {
+			string state = "1|1|1|1|1|1|1|1";
+			int action = 10;
+
+			UpdateMatrix (state, action);
+
+			Debug.Log ("UPDATED MATRIX");
+		}
+
+		if (Input.GetKeyDown (KeyCode.F2)) {
+			string state = "1|1|1|1|1|1|1|1";
+
+			if (playerMatrix.ContainsKey(state)) {
+				int[] actionVector = (int[])playerMatrix[state];
+				foreach (int i in actionVector) {
+					Debug.Log (i);
+				}
+			}
+		}
+	}
+
+	public int SelectAction() {
+//		int[] state = GetCurrentState ();
+//
+//		int action = MachineLearning.Instance.GetBestAction (state);
+//
+//		UpdateMatrix (state, action);
+//
+		return 0;
+	}
+
+	private int[] GetCurrentState() {
+		int[] t = new int[0];
+
+		return t;
+	}
+
+	public void UpdateMatrix(string state, int action) {
+		if (!playerMatrix.ContainsKey (state)) {
+			int[] actionVector = new int[72];
+			actionVector [action] += 1;
+			playerMatrix.Add (state, actionVector);
+		}
+		else {
+			int[] actionVector = (int[])playerMatrix [state];
+			actionVector [action] += 1;
+			playerMatrix [state] = actionVector;
+		}
+	}
+}
---
+++ Assets/Code/MachineLearning/MachineLearning.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MachineLearning : MonoBehaviour {
+
+	private Hashtable trainMatrix = new Hashtable ();
+
+	public static MachineLearning Instance;
+
+	private void Awake() {
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+		else {
+			Destroy (gameObject);
+		}
+
+		LoadMatrix ();
+	}
+
+	public void AddValues(Hashtable playerMatrix, bool winner) {
+		//UPDATE TRAIN MATRIX ADDING OR SUBSTRACTING PLAYER MATRIX VALUES;
+	}
+
+	public int GetBestAction(int[] state) {
+		//SEARCH THE BEST ACTION AVAILABLE FOR THE STATE PASSED;
+		return 0;
+	}
+
+	private void LoadMatrix() {
+		//LOAD TRAIN MATRIX FROM MEMORY;
+	}
+
+	private void SaveMatrix() {
+		//SAVE TRAIN MATRIX IN MEMORY;
+	}
+}
